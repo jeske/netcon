95b0492a2371fb19728babe0145695ebbc0cd7a1
jeske <jeske>
2003-08-26 08:26:40 +0000

    improve UI for active incidents

--- nc_server/tmpl/actinclist.cs
+++ nc_server/tmpl/actinclist.cs
@@ -3,9 +3,9 @@
 <table border=1>
   <tr><td>Incident</td><td>State</td><td>First Event</td><td>Last Change</td><td>Name</td></tr>
   <?cs each:inc = CGI.active_incidents ?>
-  <tr <?cs if:inc.incident_id==Query.incident_id ?>BGCOLOR=#FFCCCC<?cs /if ?>>
+  <tr>
       <td align=center><a href="index.py?incident_id=<?cs var:inc.incident_id ?>">&nbsp;<?cs var:inc.incident_id ?>&nbsp;</a></td>
-      <td><?cs var:IncidentStates[inc.state.enum] ?></td>
+      <td <?cs if:(inc.state.enum=="new") || (inc.state.enum=="viewed") ?>BGCOLOR=#FFCCCC<?cs /if ?>><?cs var:IncidentStates[inc.state.enum] ?></td>
       <td><?cs var:inc.start.string ?></td>
       <td><?cs var:inc.end.string ?></td>
       <td><b><?cs var:inc.name ?></b></td>
